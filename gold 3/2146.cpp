c18bb1cd653432cf77b7c291bf29d18c0b373dff
kocanory <kocan9803@naver.com>
2024-03-20 15:35:08 +0900

    2146
    - bfs
    - 섬 별로 라벨링한다.
    - 각 섬 별로 다음 섬까지 가는 거리의 최솟값을 구하고 갱신한다.

--- gold 3/2146.cpp
+++ gold 3/2146.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #include <tuple>
 #include <climits>
 
 using namespace std;
 
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
 vector<vector<int>> arr;
 vector<vector<bool>> visited;
 int n, answer = INT_MAX;
 
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-
-void bfs(int num){
-    queue<tuple<int, int, int>> q;
+void findIsland(int i, int j, int label){
+    queue<tuple<int, int>> q;
+    q.push({i, j});
+    arr[i][j] = label;
     
+    while(!q.empty()){
+        auto[x, y] = q.front(); q.pop();
+        
+        for(int i = 0;i < 4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if(arr[nx][ny] == 0 || arr[nx][ny] == label) continue;
+            arr[nx][ny] = label;
+            q.push({nx, ny});
+        }
+    }
+}
+
+void bfs(int label){
+    queue<tuple<int, int, int>> q;
     for(int i = 0;i < n;i++){
         for(int j = 0;j < n;j++){
-            if(arr[i][j] == num){
+            if(arr[i][j] == label){
                 visited[i][j] = true;
                 q.push({0, i, j});
             }
@@ -32,34 +52,14 @@ void bfs(int num){
             int ny = y + dy[i];
             
             if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if(arr[nx][ny] != 0 && arr[nx][ny] != num){
+            if(arr[nx][ny] != 0 && arr[nx][ny] != label){
                 answer = min(answer, cost);
                 return;
             }
             if(visited[nx][ny]) continue;
-            q.push({cost + 1, nx, ny});
+            
             visited[nx][ny] = true;
-        }
-    }
-}
-
-void findLand(int x, int y, int label){
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    arr[x][y] = label;
-    
-    while(!q.empty()){
-        pair<int, int> now = q.front(); q.pop();
-        
-        for(int i = 0;i < 4;i++){
-            int nx = now.first + dx[i];
-            int ny = now.second + dy[i];
-            
-            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if(arr[nx][ny] == 0 || arr[nx][ny] == label) continue;
-            
-            q.push({nx, ny});
-            arr[nx][ny] = label;
+            q.push({cost + 1, nx, ny});
         }
     }
 }
@@ -67,20 +67,19 @@ void findLand(int x, int y, int label){
 void solution(){
     int label = 2;
     for(int i = 0;i < n;i++){
-        for(int j =0;j < n;j++){
-            if(arr[i][j]){
-                findLand(i, j, label);
+        for(int j = 0;j < n;j++){
+            if(arr[i][j] == 1){
+                findIsland(i, j, label);
                 label++;
             }
         }
     }
     
-    for(label;label >= 2;label--)
-    {
-        visited.resize(n, vector<bool>(n, false));
+    for(label;label >= 2;label--){
+        visited.resize(n, vector<bool> (n, false));
         bfs(label);
     }
-    cout << answer;
+    cout << answer << "\n";
 }
 
 int main()
@@ -89,14 +88,11 @@ int main()
     cin.tie(NULL);
     
     cin >> n;
-    
     arr.resize(n, vector<int>(n));
-    
     for(int i = 0;i < n;i++)
         for(int j = 0;j < n;j++)
             cin >> arr[i][j];
-
+            
     solution();
-    
     return 0;
 }
