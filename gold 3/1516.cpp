3037135a3f980f700ff1b439245e24d31d4eaece
Lee SeungHyeon <kocan9803@naver.com>
2024-03-22 16:56:38 +0900

    1516
    - 위상정렬
    - 건물을 짓는데 걸리는 시간과 해당 노드에 들어오는 진입 차수를 입력 받는다.
    - 진입 차수가 0인 노드를 queue에 넣는다.
    - queue에 저장된 노드를 순회하면서 순회 중인 노드에 오는데 걸리는 시간과 현재 노드에서 걸리는 시간의 합(다음 노드까지 가는데 걸리는 시간)의 최댓값을 갱신하고, 다음 노드의 진입 차수를 1 감소한다. 만약 진입차수가 0이 되면 queue에 넣고 순회를 계속한다.
    - 마지막에는 현재 노드까지 오는 시간과 현재 노드에서 걸리는 시간을 합해서 출력한다.

--- gold 3/1516.cpp
+++ gold 3/1516.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 #include <queue>
 
 using namespace std;
-
-unordered_map<int, vector<int>> graph;
-vector<int> inDeg;
 
 int main()
 {
@@ -16,44 +13,37 @@ int main()
     int n;
     cin >> n;
     
+    unordered_map<int, vector<int>> graph;
     vector<int> t(n + 1);
     vector<int> dp(n + 1, 0);
-    inDeg.resize(n + 1, 0);
+    vector<int> inDeg(n + 1, 0);
+    queue<int> q;
+    
     
     for(int i = 0;i < n;i++){
-        bool flag = true;
-        int input;
+        cin >> t[i + 1];
+        
         while(true){
+            int input;
             cin >> input;
             if(input == -1) break;
-            if(flag){
-                flag = false;
-                t[i + 1] = input;
-            }
-            else{
-                graph[input].push_back(i + 1);
-                inDeg[i + 1]++;
-            }
+            graph[input].push_back(i + 1);
+            inDeg[i + 1]++;
         }
     }
     
-    queue<int> q;
     for(int i = 1;i <= n;i++)
-        if(inDeg[i] == 0) 
-            q.push(i);
-
-            
+        if(inDeg[i] == 0) q.push(i);
+        
     while(!q.empty()){
         int now = q.front(); q.pop();
-        
         for(auto next : graph[now]){
             dp[next] = max(dp[next], dp[now] + t[now]);
             inDeg[next]--;
-            if(inDeg[next] == 0){
-                q.push(next);
-            }
+            if(inDeg[next] == 0) q.push(next);
         }
     }
+    
     for(int i = 1;i <= n;i++)
         cout << dp[i] + t[i] << "\n";
     return 0;
