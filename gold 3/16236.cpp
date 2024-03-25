ca55abaefb9b85b3a402eb98e5daf02a344bbf46
Lee SeungHyeon <kocan9803@naver.com>
2024-03-25 21:26:42 +0900

    16236
    - bfs
    - 먹을 수 있는 물고기를 하나 씩 탐색한다.
    - 다음 위치에 있는 물고기를 먹을 수 있는 경우에는 아기 상어를 그 위치로 이동시키고, 이동 횟수를 더한다.
    - 물고기를 지나쳐야 하는 경우 큐에 넣고 탐색한다.
    - 만약 같은 움직임 횟수에서 현재 위치보다 위쪽, 왼쪽에 물고기가 있으면 그 위치로 이동시킨다.
    - 현재 탐색 횟수가 물고기를 먹는데 움직인 횟수보다 커지면 탐색을 건너뛴다.

--- gold 3/16236.cpp
+++ gold 3/16236.cpp
@@ -1,47 +1,44 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <tuple>
+#include <vector>
 
 using namespace std;
 
-int dx[] = {-1, 0, 0, 1};
-int dy[] = {0, -1, 1, 0};
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
 
-int n;
 vector<vector<int>> arr;
 vector<vector<bool>> visited;
-int bx, by, result = 0, sz = 2, eat_count = 0;
+int n, sx, sy, result = 0, sz = 2, e_count = 0;
 bool eat = false;
 
-void bfs(int i, int j, int size){
+void bfs(int i, int j){
     queue<tuple<int, int, int>> q;
     q.push({i, j, 0});
     visited[i][j] = true;
-    int t;
+    int move = -1;
     
     while(!q.empty()){
         auto[x, y, count] = q.front(); q.pop();
         
-        if(arr[x][y] > 0 && arr[x][y] < size && t == count){
-            if((bx > x) || (bx == x && by > y)){
-                bx = x;
-                by = y;
+        if(eat && move < count) continue;
+        
+        if(arr[x][y] > 0 && arr[x][y] < sz && move == count){
+            if((sx > x) || (sx == x && sy > y)){
+                sx = x, sy = y;
             }
         }
         
         for(int i = 0;i < 4;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
+            int nx = x + dx[i], ny = y + dy[i];
             if(nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) continue;
-            if(arr[nx][ny] <= size){
-                if(arr[nx][ny] > 0 && arr[nx][ny] < size && !eat){
+            if(arr[nx][ny] <= sz){
+                if(arr[nx][ny] > 0 && arr[nx][ny] < sz && !eat){
                     eat = true;
-                    bx = nx;
-                    by = ny;
-                    t = count + 1;
-                    result += t;
+                    sx = nx, sy = ny;
+                    move = count + 1;
+                    result += move;
                 }
                 else{
                     q.push({nx, ny, count + 1});
@@ -49,6 +46,7 @@ void bfs(int i, int j, int size){
                 }
             }
         }
+        
     }
 }
 
@@ -58,29 +56,28 @@ int main()
     cin.tie(NULL);
     
     cin >> n;
-    arr.resize(n, vector<int> (n));
+    
+    arr.assign(n, vector<int>(n));
     
     for(int i = 0;i < n;i++)
-        for(int j =0;j < n;j++)
-        {
+        for(int j = 0;j < n;j++){
             cin >> arr[i][j];
             if(arr[i][j] == 9){
-                bx = i;
-                by = j;
                 arr[i][j] = 0;
+                sx = i, sy = j;
             }
         }
 
     while(true){
-        visited.assign(n, vector<bool> (n, false));
-        bfs(bx, by, sz);
+        visited.assign(n, vector<bool>(n, false));
+        bfs(sx, sy);
         if(eat){
             eat = false;
-            eat_count++;
-            arr[bx][by] = 0;
-            if(eat_count == sz){
+            arr[sx][sy] = 0;
+            e_count++;
+            if(e_count == sz){
                 sz++;
-                eat_count = 0;
+                e_count = 0;
             }
         }
         else break;
