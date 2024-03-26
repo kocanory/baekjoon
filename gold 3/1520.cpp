78f774ed67eb8bfa07759c97d7043efb6634d5ac
kocanory <kocan9803@naver.com>
2024-03-26 16:32:25 +0900

    1520
    - dfs, 메모이제이션
    - 4가지 방향에 대해서 현재 자리보다 다음 자리가 값이 더 작으면 다음 자리를 탐색한다.
    - 각 자리에 대한 값을 저장하여 향후 다시 탐색할 때 그 값을 이용하게 한다.

---
+++ gold 3/1520.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n, m;
+vector<vector<int>> arr;
+vector<vector<int>> dp;
+
+int dfs(int x, int y){
+    if(x == n - 1 && y == m - 1) return 1;
+    if(dp[x][y] != -1) return dp[x][y];
+    
+    dp[x][y] = 0;
+    for(int i = 0;i < 4;i++){
+        int nx = x + dx[i], ny = y + dy[i];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if(arr[x][y] > arr[nx][ny]) dp[x][y] += dfs(nx, ny);
+    }
+    return dp[x][y];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> m;
+    
+    arr.assign(n + 1, vector<int>(m + 1, 0));
+    dp.assign(n + 1, vector<int>(m + 1, -1));
+    
+    for(int i = 0;i < n;i++)
+        for(int j =0;j < m;j++)
+            cin >> arr[i][j];
+    
+    cout << dfs(0, 0) << "\n";
+    return 0;
+}
