ea1dc21873d01dba761c70da17dca043d09e05bd
kocanory <kocan9803@naver.com>
2024-03-30 18:01:17 +0900

    11049
    - 연쇄행렬 최소곱셈 알고리즘(matrix chain multiplication)
    - 결합 형태에 따라서 두 행렬의 개수와 행렬 간 곰셈 횟수의 합을 구한 후 최솟값을 갱신한다.
    - 곱하는 행렬의 개수가 0 ~ n 순으로 진행된다.

--- gold 3/11049.cpp
+++ gold 3/11049.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int n;
-vector<vector<int>> arr;
+vector<vector<int>> matrix;
 vector<vector<int>> dp(510, vector<int>(510, INT_MAX));
 
 int mat(int n){
@@ -15,7 +15,7 @@ int mat(int n){
             if(a == b) dp[a][b] = 0;
             else{
                 for(int k = a;k < b;k++)
-                    dp[a][b] = min(dp[a][b], dp[a][k] + dp[k + 1][b] + (arr[a][0] * arr[k][1] * arr[b][1]));
+                    dp[a][b] = min(dp[a][b], dp[a][k] + dp[k + 1][b] + (matrix[a][0] * matrix[k][1] * matrix[b][1]));
             }
         }
     }
@@ -24,12 +24,13 @@ int mat(int n){
 
 int main()
 {
-    cin >> n;
-    arr.assign(n, vector<int>(2));
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    cin >> n;
+    matrix.assign(n, vector<int>(2));
     for(int i = 0;i < n;i++)
-        cin >> arr[i][0] >> arr[i][1];
-        
+        cin >> matrix[i][0] >> matrix[i][1];
     cout << mat(n) << "\n";
     return 0;
 }
