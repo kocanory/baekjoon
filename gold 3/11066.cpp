8d1868d291e91f96b247b1d8dbb494625249dc5f
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-03-31 21:33:03 +0900

    Update 11066.cpp
    - dp
    - 파일들의 분할 지점에 따라 각 분할된 파일들끼리 합치는데 걸리는 시간의 합(dp[a][l] + dp[l + 1][b])과 분할된 파일들 간 합치는데 걸리는 시간의 합(sum[b + 1] - sum[a])을 합하여 배열에 저장한다.
    - 배열을 초기화할 때 0으로 초기화해야 한다.(각 파일 하나는 합치는 시간이 0이다.)

--- gold 3/11066.cpp
+++ gold 3/11066.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-int t, k, a;
+int t, k, n;
 vector<int> sum;
 vector<vector<int>> dp;
 
 int calc(){
-    for(int i = 1;i < k;i++){
+    for(int i = 0;i < k;i++){
         for(int j = 0;j < k - i;j++){
             int a = j, b = j + i;
-            dp[a][b] = INT_MAX;
-            for(int l = a;l < b;l++)
-                dp[a][b] = min(dp[a][b], dp[a][l] + dp[l + 1][b] + sum[b + 1] - sum[a]);
+            if(a == b) dp[a][b] = 0;
+            else{
+                for(int l = a;l < b;l++)
+                    dp[a][b] = min(dp[a][b], dp[a][l] + dp[l + 1][b] + sum[b + 1] - sum[a]);
+            }
         }
     }
     return dp[0][k - 1];
@@ -28,14 +30,15 @@ int main()
     cin >> t;
     while(t--){
         cin >> k;
-        dp.assign(k, vector<int>(k, 0));
         sum.assign(k + 1, 0);
-        for(int i = 0;i < k;i++)
-        {
-            cin >> a;
-            sum[i + 1] = sum[i] + a;
+        dp.assign(k, vector<int>(k, INT_MAX));
+        
+        for(int i = 0;i < k;i++){
+            cin >> n;
+            sum[i + 1] = sum[i] + n;
         }
         cout << calc() << "\n";
     }
+
     return 0;
 }
