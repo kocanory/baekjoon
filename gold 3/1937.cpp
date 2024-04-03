c221f458c9ced713c97899a534d2fcf9844e753c
kocanory <kocan9803@naver.com>
2024-04-03 15:48:32 +0900

    1937
    - dp, dfs
    - 인접해있는 대나무의 개수가 더 크다면 그곳으로 이동하고, 경로 값을 벡터에 저장한다.
    - 이미 경로 값이 벡터에 저장되어 있다면 그 값을 그대로 이용한다.
    - 마지막 지점을 판별할 때는 인접해있는 대나무 개수가 현재 위치 대나무 개수보다 작으면 마지막 지점이다.

---
+++ gold 3/1937.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n, result = 1;
+vector<vector<int>> arr;
+vector<vector<int>> dp;
+
+int dfs(int x, int y){
+    bool flag = true;
+    if(dp[x][y] != -1) return dp[x][y];
+    for(int d = 0;d < 4 && flag;d++){
+        int nx = x + dx[d], ny = y + dy[d];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if(arr[x][y] < arr[nx][ny]) flag = false;
+    }
+    if(flag) return dp[x][y] = 1;
+    dp[x][y] = 1;
+    int next = 0;
+    for(int d = 0;d < 4;d++){
+        int nx = x + dx[d], ny = y + dy[d];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if(arr[x][y] < arr[nx][ny] )
+            next = max(next, dfs(nx, ny));
+    }
+    return dp[x][y] = dp[x][y] + next;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n;
+    arr.assign(n, vector<int>(n));
+    dp.assign(n, vector<int>(n, -1));
+    
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < n;j++)
+            cin >> arr[i][j];
+            
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < n;j++)
+            if(dp[i][j] == -1)
+                result = max(result, dfs(i, j));
+    
+    cout << result << "\n";
+    return 0;
+}
