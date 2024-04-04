6748be07d170a16ea910a41683319961090072de
kocanory <kocan9803@naver.com>
2024-04-04 20:47:35 +0900

    10986
    - 누적합(1 ~ j)을 구한 후 m으로 나누어 떨어지는지 확인한다.
    - 누적합에서 m으로 나눈 나머지에 따라 카운트를 저장하고, 카운트가 2 이상인 경우(나머지가 같은 지점이 여러개) 지점을 두 개 고를 수 있는 경우의 수를 구하고 더한다.(나머지가 같은 구간 사이는 0이 되기 때문이다.)

--- gold 3/10986.cpp
+++ gold 3/10986.cpp
@@ -2,38 +2,33 @@
 #include <vector>
 
 using namespace std;
-
 int n, m;
 long long result = 0;
-vector<long long> S, C;
+
+vector<long long> sum, count;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> n >> m;
-    
-    S.assign(n, 0L);
-    C.assign(m, 0L);
-    
-    cin >> S[0];
-    for(int i = 1;i < n;i++)
-    {
-        int input;
-        cin >> input;
-        S[i] = S[i - 1] + input;
-    }
+    sum.assign(n, 0);
+    count.assign(m, 0);
     
     for(int i = 0;i < n;i++){
-        int r = S[i] % m;
-        if(r == 0) result++;
-        C[r]++;
+        int input;
+        cin >> input;
+        if(i > 0) sum[i] = sum[i - 1] + input;
+        else sum[i] = input;
+        
+        if(sum[i] % m == 0) result++;
+        count[sum[i] % m]++;
     }
     
-    for(int i = 0;i < m;i++){
-        if(C[i] > 1) result += (C[i] * (C[i] - 1) / 2);
-    }
+    for(int i = 0;i < m;i++)
+        if(count[i] > 1) result += (count[i] * (count[i] - 1) / 2);
+        
     cout << result << "\n";
     return 0;
 }
