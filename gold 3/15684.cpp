563e4f8db1e1e94507a67923a05433612cbf1c20
kocanory <kocan9803@naver.com>
2024-04-05 17:53:15 +0900

    15684
    - 완전 탐색
    - 가로선이 없는 곳에 가로선을 놓고, i번 세로선의 결과가 i번이 나오는지 확인한다.

--- gold 3/15684.cpp
+++ gold 3/15684.cpp
@@ -3,12 +3,11 @@
 #include <climits>
 
 using namespace std;
-
-int n, m, h, result = INT_MAX;
-vector<vector<int>> map;
 vector<vector<bool>> visited;
 
-bool ladder(){
+int n, m, h, result = INT_MAX;
+
+bool checkLadder(){
     for(int i = 1;i <= n;i++){
         int now = i;
         for(int j = 1;j <= h;j++){
@@ -20,18 +19,18 @@ bool ladder(){
     return true;
 }
 
-void select(int index, int count){
+void select(int line, int count){
     if(count > 3) return;
-    if(ladder()){
+    if(checkLadder()){
         result = min(result, count);
         return;
     }
     
-    for(int i = index;i <= h;i++){
+    for(int i = line;i <= h;i++){
         for(int j = 1;j < n;j++){
             if(visited[j][i]) continue;
-            if(visited[j - 1][i]) continue;
             if(visited[j + 1][i]) continue;
+            if(visited[j - 1][i]) continue;
             
             visited[j][i] = true;
             select(i, count + 1);
@@ -44,18 +43,18 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     cin >> n >> m >> h;
-    map.assign(n + 1, vector<int>(m + 1));
-    visited.assign(n + 1, vector<bool>(m + 1, false));
-    for(int i = 0;i < m;i++)
-    {
+    visited.assign(n + 1, vector<bool>(h + 1, false));
+    
+    for(int i = 0;i < m;i++){
         int a, b;
         cin >> a >> b;
         visited[b][a] = true;
     }
     
     select(1, 0);
+    
     if(result == INT_MAX) result = -1;
     cout << result << "\n";
     return 0;
