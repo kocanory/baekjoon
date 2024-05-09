3b89b6c7ca5f2e607b8740415102718f106616f9
kocanory <kocan9803@naver.com>
2024-05-09 19:08:34 +0900

    1562
    - dfs, 비스마스크
    - 0과 9가 포함되어 있다면 그 사이에 있는 수 또한 포함되어 있음. 따라서 0과 9를 탐색했는지 비트마스크를 사용해서 판별
    - n번째 자리까지 탐색했을 때 비트가 3이라면(9와 0 탐색했다면 비트에 각각 2, 1 추가) 1을 반환하고 되돌아가면서 더해나간다.
    - 9를 탐색했다면 비트에 2를 추가하고, 0을 탐색했다면 비트에 1을 추가한다.

---
+++ gold 1/1562.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n, answer = 0;
+int dp[10][100][4] = {0, };
+
+
+int dfs(int num, int idx, int bit){
+    int &ret = dp[num][idx][bit];
+    if(ret) return ret;
+    if(idx >= n){
+        if(bit == 3) return 1;
+        else return 0;
+    }
+    if(num < 9){
+        ret += dfs(num + 1, idx + 1, bit | ((num + 1 == 9) ? 2 : 0));
+        ret %= 1000000000;
+    }
+    if(num > 0){
+        ret += dfs(num - 1, idx + 1, bit | ((num - 1 == 0) ? 1 : 0));
+        ret %= 1000000000;
+    }
+    return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for(int i = 1;i <= 9;i++){
+        answer += dfs(i, 1, i == 9 ? 2 : 0);
+        answer %= 1000000000;
+    }
+    cout << answer << "\n";
+    return 0;
+}
