bfe9deb42bdf60c5bb3fe6a7d8fe4d6a27f2b51b
kocanory <kocan9803@naver.com>
2024-05-11 17:21:04 +0900

    9252
    - dp
    - 두 문자열에서 현재 탐색하는 문자가 서로 같다면 현재 위치에 왼쪽 대각선의 값 + 1을 저장한다.
    - 문자가 다르다면 현재 위치의 상단, 왼쪽 값 중 가장 큰 값을 저장한다.
    - 공통 부분 수열을 출력할 때에는 dp 벡터에서 백트래킹을 진행한다. 문자가 서로 같을 때는 문자를 붙이고 대각선으로 이동. 그렇지 않으면 상단 값과 왼쪽 값 중 가장 큰 쪽으로 이동한다.

--- gold 4/9252.cpp
+++ gold 4/9252.cpp
@@ -10,7 +10,7 @@ vector<vector<int>> dp;
 void lcs(){
     for(int i = 1;i <= n;i++){
         for(int j = 1;j <= m;j++){
-            if(s1[i] == s2[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
             else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }  
@@ -18,7 +18,7 @@ void lcs(){
 
 string backtracking(int i, int j){
     if(i == 0 || j == 0) return "";
-    else if(s1[i] == s2[j]) return backtracking(i - 1, j - 1) + s1[i];
+    if(s1[i - 1] == s2[j - 1]) return backtracking(i - 1, j - 1) + s1[i - 1];
     
     if(dp[i][j - 1] >= dp[i - 1][j]) return backtracking(i, j - 1);
     else return backtracking(i - 1, j);
@@ -33,9 +33,6 @@ int main()
     n = s1.length();
     m = s2.length();
     dp.assign(n + 1, vector<int>(m + 1, 0));
-    
-    s1 = " " + s1;
-    s2 = " " + s2;
     
     lcs();
     
