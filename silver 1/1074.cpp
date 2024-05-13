1753e5d16c14b3c397bd8c9b7655ff033b5e7b5e
kocanory <kocan9803@naver.com>
2024-05-13 18:50:25 +0900

    1074
    - 분할정복
    - 찾고자 하는 좌표가 현재 지점의 범위안에 있다면 4개의 범위로 분할하여 탐색한다. 만약에 범위 안에 없다면 현재 범위 사이즈를 더한다.

---
+++ silver 1/1074.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n, r, c, answer = 0;
+
+void dfs(int x, int y, int size){
+    if(x == r && y == c){
+        cout << answer << "\n";
+        return;
+    }
+    
+    else if(r < x + size && c < y + size && r >= x && c >= y){
+        dfs(x, y, size / 2);
+        dfs(x, y + size / 2, size / 2);
+        dfs(x + size / 2, y, size / 2);
+        dfs(x + size / 2, y + size / 2, size / 2);
+    }
+    else
+        answer += (size * size);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> r >> c;
+    dfs(0, 0, pow(2, n) * pow(2, n));
+    return 0;
+}
