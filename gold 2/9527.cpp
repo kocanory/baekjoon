d6b064df687871b86c42030131466de6d9108258
kocanory <kocan9803@naver.com>
2024-05-31 18:46:43 +0900

    9527
    - 최상위 비트가 i번 이하인 모든 수의 1의 개수 합을 누적합으로 구한다.
    - 최상위 비트부터 순회하면서 i번 비트가 1인지 확인한다. 예를 들면 4번 비트가 1이면 10000(2) 미만 모든 수의 1 개수와 4번 비트가 1인 수의 개수를 더한다. 그 후 3번 비트부터 다시 순회하면서 비트가 1인 곳을 찾으면 그 비트가 1인 수의 개수와 그 비트가 1인 수 미만의 수 개수를 구한다. 이 과정을 반복한다.
    - 주어진 수가 홀수인 경우 카운트에 1을 더한다. 이는 계산 과정에서 1번째 비트를 탐색할 때 01, 10을 탐색하게 되므로 11을 카운트하지 못한다. 따라서 홀수는 처음에 따로 처리한다.

--- gold 2/9527.cpp
+++ gold 2/9527.cpp
@@ -4,30 +4,29 @@
 using namespace std;
 
 long long a, b;
-vector<long long> arr(64);
+vector<long long> power2(64);
 
 long long check(long long n){
     long long ret = n & 1;
-
+    
     for(int i = 63;i > 0;i--){
         if(n & (1LL << i)){
-            ret += arr[i - 1] + (n - (1LL << i) + 1);
+            ret += power2[i - 1] + (n - (1LL << i) + 1);
             n -= 1LL << i;
         }
     }
-
     return ret;
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> a >> b;
-
-    arr[0] = 1;
-    for(int i = 1;i < 64;i++) arr[i] = 2 * arr[i - 1] + (1LL << i);
-
+    
+    power2[0] = 1;
+    for(int i = 1;i < 64;i++) power2[i] = power2[i - 1] * 2 + (1LL << i);
     cout << check(b) - check(a - 1) << "\n";
     return 0;
 }
