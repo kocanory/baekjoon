2f8d927c8a1fc6f1f4ca2f0f38c499d1c8ee31d8
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-06-04 15:53:23 +0900

    16556
    - 유니온 파인드
    - upper_bound 함수를 사용해서 철수가 낼 카드보다 큰 카드를 구하고, 유니온 파인드를 사용해서 구한 카드의 인덱스와 그 다음 인덱스를 연결하여 이전에 낸 카드를 다시 내지 않고 그 다음으로 큰 카드를 내도록 한다.

--- platinum 5/16566.cpp
+++ platinum 5/16566.cpp
@@ -7,37 +7,38 @@ using namespace std;
 int n, m, k, num;
 vector<int> card, parent;
 
-int findParent(int i){
+int find(int i){
     if(parent[i] == i) return i;
-    return parent[i] = findParent(parent[i]);
+    return parent[i] = find(parent[i]);
 }
 
 void join(int a, int b){
-    a = findParent(a), b = findParent(b);
+    a = find(a), b = find(b);
     parent[min(a, b)] = max(a, b);
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> n >> m >> k;
-    parent.assign(m + 1, 0);
+    parent.assign(m, 0);
     for(int i = 0;i < m;i++) parent[i] = i;
-
+    
     for(int i = 0;i < m;i++){
         cin >> num;
         card.push_back(num);
     }
-
+    
     sort(card.begin(), card.end());
-
-
-    for(int i = 0;i < k;i++){
+    
+    for(int i = 0;i < k ;i++){
         cin >> num;
         int index = upper_bound(card.begin(), card.end(), num) - card.begin();
-        cout << card[findParent(index)] << "\n";
-        join(findParent(index), findParent(index) + 1);
+        cout << card[find(index)] << "\n";
+        if(index < m - 1) join(find(index), find(index) + 1);
     }
+
     return 0;
 }
