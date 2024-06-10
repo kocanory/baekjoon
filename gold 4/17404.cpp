7ac36d72000bedec901f011b57b0dde1bbc89bdf
kocanory <kocan9803@naver.com>
2024-06-10 19:49:30 +0900

    17404
    - dp
    - 첫 집의 색상과 마지막 집의 색상이 겹치면 안되므로 첫 집의 각각 색상에 대해서 경우의 수를 구해야 한다.
    - 첫 집이 r인 경우 마지막 집이 g, b 인 경우의 수 중 작은 값을 반환하고, 첫 집이 g인 경우 마지막 집이 r, b 인 경우의 수 중 작은 값을, 첫 집이 b인 경우 마지막 집이 r, g 중 작은 값을 반환한다.

--- gold 4/17404.cpp
+++ gold 4/17404.cpp
@@ -17,38 +17,22 @@ int main(){
 
     for(int i = 1;i <= n;i++) cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 
-    dp[1][0] = arr[1][0];
-    dp[1][1] = dp[1][2] = 1e9;
+    for(int color = 0;color < 3;color++){
+        for(int i = 0;i < 3;i++){
+            if(i == color) dp[1][i] = arr[1][i];
+            else dp[1][i] = 1e9;
+        }
+        for(int i = 2;i <= n;i++){
+            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        }
 
-    for(int i = 2;i <= n;i++){
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        for(int i = 0;i < 3;i++){
+            if(i == color) continue;
+            answer = min(answer, dp[n][i]);
+        }
     }
-
-    answer = min(answer, min(dp[n][1], dp[n][2]));
-
-    dp[1][1] = arr[1][1];
-    dp[1][0] = dp[1][2] = 1e9;
-
-    for(int i = 2;i <= n;i++){
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
-    }
-
-    answer = min(answer, min(dp[n][0], dp[n][2]));
-
-    dp[1][2] = arr[1][2];
-    dp[1][0] = dp[1][1] = 1e9;
-
-    for(int i = 2;i <= n;i++){
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
-    }
-
-    answer = min(answer, min(dp[n][0], dp[n][1]));
 
     cout << answer << "\n";
 
