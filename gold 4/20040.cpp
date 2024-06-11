042bb17ad5fcbdeefcb260e06e5bb3bc7910f2e1
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-06-11 18:35:34 +0900

    20040
    - 유니온 파인드
    - 입력 받은 두 점의 부모가 같다면 한 무리로 이루어져 있는 것(사이클 형성). 따라서 현재 순서를 저장하고 탐색 종료.
    - 부모가 다르다면 두 점의 부모를 서로 연결한다.

---
+++ gold 4/20040.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int n, m, answer = 0;
+vector<int> parent;
+
+int find(int i){
+    if(parent[i] == i) return i;
+    return parent[i] = find(parent[i]);
+}
+
+void join(int a, int b){
+    a = find(a), b = find(b);
+    parent[max(a, b)] = min(a, b);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    parent.assign(n, 0);
+    for(int i = 0;i < n;i++) parent[i] = i;
+
+    for(int i = 0;i < m;i++){
+        int a, b;
+        cin >> a >> b;
+        if(find(a) == find(b)){
+            answer = i + 1;
+            break;
+        }
+        join(find(a), find(b));
+    }
+    cout << answer << "\n";
+    return 0;
+}
