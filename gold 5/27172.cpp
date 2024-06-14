a7f497863b936596d8ab56386c840ed5a41f6fbc
kocanory <kocan9803@naver.com>
2024-06-14 17:56:28 +0900

    27172
    - 카드를 입력 받고, 카드 번호의 입력 여부를 map에 저장한다.
    - 카드 번호를 순회하면서 에라토스테네스의 체를 활용하여 현재 순회 중인카드 번호의 배수 중 입력 받은 번호가 있는지 확인한다. 만약 있으면 점수를 증가시키고, 배수인 번호는 점수를 감소시킨다.
    - 모든 카드를 순회했다면 점수를 출력한다.

---
+++ gold 5/27172.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+vector<int> arr, score(1000001);
+unordered_map<int, bool> cards;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n, 0);
+
+    for(int i = 0;i < n;i++) {
+        cin >> arr[i];
+        cards[arr[i]] = true;
+    }
+
+    for(auto &a : arr){
+        for(int i = 2 * a;i <= 1000000;i += a){
+            if(cards[i]){
+                score[a]++;
+                score[i]--;
+            }
+        }
+    }
+
+    for(auto &a : arr) cout << score[a] << " ";
+    return 0;
+}
