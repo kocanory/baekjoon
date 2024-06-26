65aa3e70561899e9a64b005ab20fcbb49ae39912
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-06-26 15:03:33 +0900

    1561
    - 이분 탐색(파라메트릭 서치)
    - 0 ~ 모든 사람이 놀이 기구를 최대로 타는 시간의 중간 값을 구해서 그 시간 안에 놀이기구를 n명이 모두 탈 수 있는 지 확인한다. 만약 모두 탈 수 있다면 right를 줄이고, 이때 mid 값을 저장한다. 모두 탈 수 없다면 left를 늘린다.
    - 최종적으로 n명이 타는데 걸리는 시간은 minTime에 해당한다. 따라서 minTime- 1 시간까지 탈 수 있는 사람의 수를 구하고, minTime 시간이 되었을 때 각 놀이기구 시간을 순회하면서 마지막 인원이 탈 수 있는 놀이기구를 구한다. 이는 minTime 때 탈 수 있는 놀이기구가 파편적이기 때문에, 이전 시간에 탈 수 있는 놀이기구를 구한 후  다음 시간에 탈 수 있는 놀이기구를 구하는 것이다.

--- gold 1/1561.cpp
+++ gold 1/1561.cpp
@@ -1,49 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int n, m;
-long long maxTime = 0;
-vector<int> arr;
+vector<int> t;
+long long maxTime = 0, minTime;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n >> m;
-    arr.assign(m, 0);
-
-
-    for(int i = 0 ;i < m;i++) {
-        cin >> arr[i];
-        maxTime = max(maxTime, (long long)arr[i]);
+    t.assign(m, 0);
+    
+    for(auto &a : t){
+        cin >> a;
+        maxTime = max(maxTime, (long long)a);
     }
-
+    
     if(n <= m){
         cout << n << "\n";
         return 0;
     }
-
-    long long left = 0, right = maxTime * n;
+    
+    long long left = 1, right = maxTime * n;
     while(left <= right){
         long long mid = (left + right) / 2, sum = m;
-        for(auto &a : arr) sum += (mid / a);
-        if(sum >= n) right = mid - 1;
+        for(auto &a : t) sum += (mid / a);
+        if(sum >= n){
+            right = mid - 1;
+            minTime = mid;
+        }
         else left = mid + 1;
     }
-
+    
     long long sum = m;
-    for(auto &a : arr) sum += ((left - 1) / a);
-
-    int k = n - sum, ans = 0;
-
-    while(k){
-        if(left % arr[ans] == 0) k--;
-        ans++;
+    for(auto &a : t) sum += ((minTime - 1) / a);
+    
+    for(int i = 0;i < m;i++){
+        if(minTime % t[i] == 0) sum++;
+        if(sum == n){
+            cout << i + 1 << "\n";
+            break;
+        }
     }
-    cout << ans << "\n";
     return 0;
 }
