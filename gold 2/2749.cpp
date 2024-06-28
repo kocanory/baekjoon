60766510bf8d3afe05e7d098cb978f8a477f150c
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-06-28 13:09:35 +0900

    2749
    - 피보나치 수는 {{1, 1}, {1, 0}}의 거듭 제곱 꼴로 나타낼 수 있음.
    - 입력 받은 n을 2로 나눠가면서 위 행렬의 거듭 제곱을 시행한다. 이때 n이 홀 수가 된다면 여태까지 거듭제곱한 행렬을 저장하고, 다시 거듭제곱을 시행한다.
    - https://blogshine.tistory.com/274

--- gold 2/2749.cpp
+++ gold 2/2749.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-typedef vector<vector<long long>> matrix;
-long long n;
+typedef long long ll;
+typedef vector<vector<ll>> matrix;
 
+ll n;
 matrix a = {{1, 1}, {1, 0}}, answer = {{1, 0}, {0, 1}};
 
-matrix operator*(matrix &a, matrix &b){
+matrix operator* (matrix &a, matrix &b){
     int size = a.size();
-    matrix ret(size, vector<long long>(size, 0));
-
+    matrix ret(size, vector<ll>(size, 0));
+    
     for(int i = 0;i < size;i++){
         for(int j = 0;j < size;j++){
             for(int k = 0;k < size;k++){
@@ -28,9 +29,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n;
-
     while(n){
         if(n % 2) answer = answer * a;
         a = a * a;
