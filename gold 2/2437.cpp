0a164e6080c214064d98b79e10e372e87d5f63d8
Lee SeungHyeon <44865079+kocanory@users.noreply.github.com>
2024-06-30 18:07:21 +0900

    2437
    - 추 무게를 입력 받은 후 내림차순으로 정렬한다.
    - 1 ~ 가장 무거운 추의 무게 순으로 순회하면서 해당 무게를 만들 수 있는지 확인한다. 만들 수 있으면 탐색을 계속 진행하고, 그렇지 않으면 해당 무게를 출력한다.
    - 끝까지 무게를 만들 수 있다면(1씩 순차대로 증가하는 무게가 모두 존재) 추 무게의 합을 모두 더한 값 + 1을 출력한다.

---
+++ gold 2/2437.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, sum = 0;
+vector<int> arr;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n, 0);
+    for(int i = 0;i < n;i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr.begin(), arr.end(), greater<int>());
+    for(int i = 1;i <= arr[0];i++){
+        int compare = 0;
+        for(int j = 0;j < n;j++){
+            if(arr[j] + compare > i) continue;
+            compare += arr[j];
+            if(compare == i) break;
+        }
+        if(compare != i){
+            cout << i << "\n";
+            return 0;
+        }
+    }
+    cout << sum + 1 << "\n";
+    return 0;
+}
