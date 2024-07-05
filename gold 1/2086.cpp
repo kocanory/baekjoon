f364038950949496c9d501c3a10fb4d7f4fb5f4a
kocanory <kocan9803@naver.com>
2024-07-06 00:16:28 +0900

    2086
    - 피보나치 수의 합은 Fn + 2 - F1으로 나타낼 수 있음.
    - 피보나치 수는 {{1, 1}, {1, 0}}의 거듭 제곱 꼴로 나타낼 수 있음.
    - 입력 받은 n을 2로 나눠가면서 위 행렬의 거듭 제곱을 시행한다. 이때 n이 홀 수가 된다면 여태까지 거듭제곱한 행렬을 저장하고, 다시 거듭제곱을 시행한다.
    - https://anwltjdzheldsbql.tistory.com/17

--- gold 1/2086.cpp
+++ gold 1/2086.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#define MOD 1000000000
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<vector<ll>> matrix;
 
-ll a, b, ans = 0;
-ll MOD = 1000000000;
-matrix n = {{1, 1}, {1, 0}};
+ll a, b, ans;
+matrix m = {{1, 1}, {1, 0}};
 
-matrix operator*(matrix &n1, matrix &n2){
+matrix operator *(matrix &n1, matrix &n2){
     int size = n1.size();
-    matrix ret(2, vector<ll>(2));
+    matrix ret(size, vector<ll>(size));
     for(int i = 0;i < size;i++){
-        for(int j = 0;j < size;j++){
+        for(int j = 0;j <size;j++){
             for(int k = 0;k < size;k++)
-            {
                 ret[i][j] += (n1[i][k] * n2[k][j]);
-            }
             ret[i][j] %= MOD;
         }
     }
     return ret;
 }
 
-matrix calc(matrix n1, long long count){
-    matrix ret = {{1, 0}, {0, 1}};
+ll calc(matrix n1, ll count){
+    matrix ans = {{1, 0}, {0, 1}};
     while(count){
-        if(count % 2) ret = ret * n1;
+        if(count % 2) ans = ans * n1;
         n1 = n1 * n1;
-        count /= 2;
+        count >>= 1;
     }
-    return ret;
+    return ans[0][1];
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> a >> b;
-    matrix n1 = calc(n, b + 2), n2 = calc(n, (a - 1) + 2);
-    ans = ((n1[0][1] - 1) - (n2[0][1] - 1) + MOD) % MOD;
+    ans = (calc(m, b + 2) - calc(m, (a - 1) + 2) + MOD) % MOD;
     cout << ans << "\n";
+
     return 0;
 }
