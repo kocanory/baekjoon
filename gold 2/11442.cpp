8c73fa022b4868efaf3b6c5da60e3f3bbc9eafa6
kocanory <kocan9803@naver.com>
2024-07-07 00:06:04 +0900

    11442
    f3 - f2 = f1
    f5 - f4 = f3 -> f3 + .. + fn = f1 + f2 + ... + fn-1 -> 홀수 항의 합 = f1 + f2 + ... + fn - 1 + f1 = f0 + f1 + ... + fn-1 + f1(f0 = 0이기 때문) = Sn-1 + f1
    - 피보나치 홀수 항의 합은 Sn - 1 + f0 + f1으로 나타낼 수 있음.
    - 피보나치 수는 {{1, 1}, {1, 0}}의 거듭 제곱 꼴로 나타낼 수 있음.
    - 입력 받은 n을 2로 나눠가면서 위 행렬의 거듭 제곱을 시행한다. 이때 n이 홀 수가 된다면 여태까지 거듭제곱한 행렬을 저장하고, 다시 거듭제곱을 시행한다.
    - https://anwltjdzheldsbql.tistory.com/17

--- gold 2/11442.cpp
+++ gold 2/11442.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<vector<ll>> matrix;
+const ll MOD = 1000000007;
+ll n;
 
-ll n, ans = 0;
-ll MOD = 1000000007;
 matrix m = {{1, 1}, {1, 0}};
 
-matrix operator*(matrix &n1, matrix &n2){
-    int size = n1.size();
-    matrix ret(2, vector<ll>(2));
+matrix operator*(matrix &a, matrix &b){
+    int size = a.size();
+    matrix ret(size, vector<ll>(size));
     for(int i = 0;i < size;i++){
         for(int j = 0;j < size;j++){
             for(int k = 0;k < size;k++)
-            {
-                ret[i][j] += (n1[i][k] * n2[k][j]);
-            }
+                ret[i][j] += (a[i][k] * b[k][j]);
             ret[i][j] %= MOD;
         }
     }
     return ret;
 }
 
-matrix calc(matrix n1, long long count){
-    matrix ret = {{1, 0}, {0, 1}};
+ll calc(ll count){
+    matrix ans = {{1, 0}, {0, 1}};
     while(count){
-        if(count % 2) ret = ret * n1;
-        n1 = n1 * n1;
-        count /= 2;
+        if(count % 2) ans = ans * m;
+        m = m * m;
+        count >>= 1;
     }
-    return ret;
+    return ans[0][1];
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n;
     if(n % 2 == 0) n--;
-    matrix n1 = calc(m, (n - 1) + 2);
-    ans = ((n1[0][1] - 1) + 1) % MOD;
-    cout << ans << "\n";
+    ll ans = calc((n - 1) + 2);
+    cout << (ans - 1) + 1 << "\n";
     return 0;
 }
