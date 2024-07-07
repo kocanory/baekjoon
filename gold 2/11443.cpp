bf974f61244c76c26f1f268289fa4dabce04f48c
kocanory <kocan9803@naver.com>
2024-07-07 14:46:18 +0900

    11443
    f2 - f1 = f0
    f4 - f3 = f2 -> f2 + f4 + .. + fn = f0 + f1 + ... + fn-1 -> 짝수 항의 합 = f1 + f2 + ... + fn - 1 + f0 =  Sn-1 + f0
    - 피보나치 짝수 항의 합은 Sn - 1 + f0으로 나타낼 수 있음.
    - 피보나치 수는 {{1, 1}, {1, 0}}의 거듭 제곱 꼴로 나타낼 수 있음.
    - 입력 받은 n을 2로 나눠가면서 위 행렬의 거듭 제곱을 시행한다. 이때 n이 홀 수가 된다면 여태까지 거듭제곱한 행렬을 저장하고, 다시 거듭제곱을 시행한다.
    - https://anwltjdzheldsbql.tistory.com/17

---
+++ gold 2/11443.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<vector<ll>> matrix;
+
+const ll MOD = 1000000007;
+matrix m = {{1, 1}, {1, 0}};
+ll n;
+
+matrix operator *(matrix &a, matrix &b){
+    int size = a.size();
+    matrix ret(size, vector<ll>(size));
+    
+    for(int i = 0;i < size;i++){
+        for(int j = 0;j < size;j++){
+            for(int k = 0;k < size;k++)
+                ret[i][j] += (a[i][k] * b[k][j]);
+            ret[i][j] %= MOD;
+        }
+    }
+    return ret;
+}
+
+ll calc(ll count){
+    matrix ans = {{1, 0}, {0, 1}};
+    while(count){
+        if(count % 2) ans = ans * m;
+        m = m * m;
+        count >>= 1;
+    }
+    return ans[0][1];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    if(n % 2) n--;
+    ll ans = calc((n - 1) + 2);
+    cout << (ans - 1) << "\n";
+
+    return 0;
+}
