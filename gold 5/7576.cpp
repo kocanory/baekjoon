6c1ca6a0159f3e259725032c76ea6de5623c74c3
kocanory <kocan9803@naver.com>
2024-07-14 14:13:43 +0900

    7576
    - bfs
    - 익은 토마토(1)이 심어져 있으면 그 부분의 좌표를 큐에 저장한다. 그 후 큐에 저장된 좌표를 꺼내서 그 좌표를 기준으로 4방향을 탐색하여 안 익은 토마토(0)가 있다면 1로 바꾸고, 좌표를 큐에 넣는다.
    - 큐에서 좌표를 꺼낼 때마다 일 수를 갱신한다.
    - 모든 탐색이 끝난 후 안 익은 토마토가 있다면 -1을 출력하고, 그렇지 않으면 일 수를 출력한다.

---
+++ gold 5/7576.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int n, m, ans = 0;
+vector<vector<int>> arr;
+vector<vector<bool>> visited;
+queue<tuple<int, int, int>> q;
+
+
+bool check(){
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < m;j++)
+            if(arr[i][j] == 0)
+                return false;
+    return true;
+}
+
+void bfs(){
+    while(!q.empty()){
+        auto[x, y, count] = q.front(); q.pop();
+        ans = max(ans, count);
+        for(int i = 0;i < 4;i++){
+            int nx = x + dx[i], ny = y + dy[i];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || arr[nx][ny] == -1) continue;
+            q.push({nx, ny, count + 1});
+            visited[nx][ny] = true;
+            arr[nx][ny] = 1;
+        }
+    }
+    if(check()) cout << ans << "\n";
+    else cout << -1 << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> m >> n;
+    arr.assign(n, vector<int>(m));
+    visited.assign(n, vector<bool>(m));
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < m;j++){
+            cin >> arr[i][j];
+            if(arr[i][j] == 1){
+                q.push({i, j, 0});
+                visited[i][j] = true;
+            }
+        }
+    bfs();
+    return 0;
+}
