b126efd4d8257adaae668aeda7eed8f969aed177
kocanory <kocan9803@naver.com>
2024-07-15 15:24:42 +0900

    10026
    - dfs
    - 한 벡터에는 입력을 그대로 받고, 다른 한 벡터는 (빨강 또는 초록)을 (초록 또는 빨강)으로 변환한 후 저장한다.
    - dfs를 통해서 같은 구역을 탐색하고, 한 구역의 탐색이 끝날 때 마다 카운트를 증가한다.
    - 모든 탐색이 끝난 후 카운트를 출력한다.

--- gold 5/10026.cpp
+++ gold 5/10026.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <tuple>
 
 using namespace std;
 
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
 
-int n, ans1 = 0, ans2 = 0;
+int n, ans1, ans2;
 vector<string> arr1, arr2;
 vector<vector<bool>> visited;
 
@@ -21,45 +19,42 @@ void dfs(int x, int y, vector<string> &arr){
     }
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n;
     arr1.assign(n, "");
     arr2.assign(n, "");
     visited.assign(n, vector<bool>(n));
-
-    for(int i = 0; i < n;i++) {
+    
+    for(int i = 0;i < n;i++){
         cin >> arr1[i];
         arr2[i] = arr1[i];
-        for(int j = 0;j < n;j++) {
-            if (arr1[i][j] == 'G') arr2[i][j] = 'R';
-            else arr2[i][j] = arr1[i][j];
-        }
+        for(int j = 0;j < n;j++)
+            if(arr2[i][j] == 'G') arr2[i][j] = 'R';
     }
 
     for(int i = 0;i < n;i++)
-        for(int j = 0;j < n;j++)
-        {
+        for(int j = 0;j < n;j++)        {
             if(!visited[i][j]){
                 visited[i][j] = true;
                 dfs(i, j, arr1);
                 ans1++;
-            }
+            }    
         }
-
+        
     visited.assign(n, vector<bool>(n));
     for(int i = 0;i < n;i++)
-        for(int j = 0;j < n;j++)
-        {
+        for(int j = 0;j < n;j++)        {
             if(!visited[i][j]){
                 visited[i][j] = true;
                 dfs(i, j, arr2);
                 ans2++;
-            }
+            }    
         }
-    cout << ans1 <<" " << ans2 << "\n";
+    cout << ans1 << " " << ans2 << "\n";
     return 0;
 }
