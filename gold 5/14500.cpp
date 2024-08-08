197ac035c64c64774aca6af223f0715024ebd454
kocanory <kocan9803@naver.com>
2024-08-08 22:30:06 +0900

    14500
    - dfs
    - 탐색 블럭 4개까지 탐색 할 경우 숫자의 합의 최댓값을 갱신한다.(T자 모양 제외하고 모두 탐색 가능)
    - T자 모양은 별도로 함수 구현해서 확인한다. T자 모양에 해당하는 좌표를 확인한 후 합을 구하고 갱신한다.

--- gold 5/14500.cpp
+++ gold 5/14500.cpp
@@ -10,54 +10,53 @@ int n, m, ans = 0;
 vector<vector<int>> arr;
 vector<vector<bool>> visited;
 
-void dfs(int x, int y, int count, int sum) {
-    if(count == 4) {
+void dfs(int x, int y, int count, int sum){
+    if(count == 4){
         ans = max(ans, sum);
         return;
     }
+    visited[x][y] = true;
     for(int i = 0;i < 4;i++){
         int nx = x + dx[i], ny = y + dy[i];
         if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) continue;
-        visited[nx][ny] = true;
         dfs(nx, ny, count + 1, sum + arr[nx][ny]);
-        visited[nx][ny] = false;
     }
+    visited[x][y] = false;
 }
 
-void T_check(int x, int y) {
-    for(int i = 0;i < 4;i++) {
+void T_check(int x, int y){
+    for(int i = 0;i < 4;i++){
         int nx = x + dx[i], ny = y + dy[i];
         if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-        if(i % 2) {
+        if(i % 2){
             if(nx - 1 < 0 || nx + 1 >= n) continue;
             ans = max(ans, arr[x][y] + arr[nx][ny] + arr[nx - 1][ny] + arr[nx + 1][ny]);
-        }else {
+        }else{
             if(ny - 1 < 0 || ny + 1 >= m) continue;
             ans = max(ans, arr[x][y] + arr[nx][ny] + arr[nx][ny - 1] + arr[nx][ny + 1]);
         }
     }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n >> m;
+    
     arr.assign(n, vector<int>(m));
     visited.assign(n, vector<bool>(m));
-
+    
     for(int i = 0;i < n;i++)
-        for(int j = 0;j < m;j++) {
+        for(int j = 0;j < m;j++)
             cin >> arr[i][j];
-        }
-
+            
     for(int i = 0;i < n;i++)
-        for(int j = 0;j < m;j++) {
-            visited[i][j] = true;
+        for(int j = 0;j < m;j++){
             dfs(i, j, 1, arr[i][j]);
             T_check(i, j);
-            visited[i][j] = false;
         }
     cout << ans << "\n";
     return 0;
