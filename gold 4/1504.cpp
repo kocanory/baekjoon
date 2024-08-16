ed8535c1243c76b30cd0b2e3fffb3d4d1f6f4d11
kocanory <kocan9803@naver.com>
2024-08-17 03:08:38 +0900

    1504
    - 다익스트라
    - 1 ~ v1+ v1 ~ v2 + v2 ~ n과 1 ~ v2 + v2 ~ v1 + v1 ~ n을 구한 다음 최솟값을 갱신하고, 경로가 없으면 -1을 출력한다.
    - 1을 기준으로 다익스트라 탐색 진행 후 v1, v2 까지의 거리를 구하고, v1을 기준으로 다익스트라 탐색 진행 후 n 까지의 거리, v2를 기준으로 다익스트라 탐색 진행 후 n까지의 거리를 구한 후 위와 같은 내용으로 거리를 구한다.
    - 최댓값을 1e9로 지정하는 경우 오버플로우 발생하여 이상한 값이 들어오게 된다.

--- gold 4/1504.cpp
+++ gold 4/1504.cpp
@@ -1,62 +1,60 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 #include <queue>
 
 using namespace std;
 
-int n, e, v1, v2, ans = 1e9;
+int n, e, v1, v2, ans = 1e8;
 unordered_map<int, vector<tuple<int, int>>> graph;
 vector<int> dist;
 
-void bfs(int start) {
+void bfs(int start){
     dist.assign(n + 1, 1e8);
     dist[start] = 0;
     priority_queue<tuple<int, int>> pq;
     pq.push({0, start});
-    while(!pq.empty()) {
+    
+    while(!pq.empty()){
         auto[cost, now] = pq.top(); pq.pop();
         cost = -cost;
         if(dist[now] < cost) continue;
-        for(auto &n : graph[now]) {
-            auto[next, n_cost] = n;
-            if(dist[next] > cost + n_cost) {
-                dist[next] = cost + n_cost;
-                pq.push({-(cost + n_cost), next});
+        for(auto &n : graph[now]){
+            auto[nx, ncost] = n;
+            ncost += cost;
+            if(dist[nx] > ncost){
+                dist[nx] = ncost;
+                pq.push({-ncost, nx});
             }
         }
     }
 }
 
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n >> e;
-
-    for (int i = 0; i < e; i++) {
+    for(int i = 0;i < e;i++){
         int a, b, c;
         cin >> a >> b >> c;
         graph[a].push_back({b, c});
         graph[b].push_back({a, c});
     }
-
     cin >> v1 >> v2;
-
+    
     bfs(1);
     int to_v1 = dist[v1], to_v2 = dist[v2];
-
+    
     bfs(v1);
-    int v1_to_v2 = dist[v2], v1_to_n = dist[n];
-
+    int v1_v2 = dist[v2], v1_n = dist[n];
+    
     bfs(v2);
-    int v2_to_n = dist[n];
-
-    ans = min(ans, to_v1 + v1_to_v2 + v2_to_n);
-    ans = min(ans, to_v2 + v1_to_v2 + v1_to_n);
-
-    cout << (ans >= 1e8 ? -1 : ans) << endl;
+    int v2_n = dist[n];
+    
+    ans = min(ans, min(to_v1 + v1_v2 + v2_n, to_v2 + v1_v2 + v1_n));
+    cout << (ans >= 1e8 ? -1 : ans) << "\n";
     return 0;
 }
