5cec6f42caee3f5634c18dba8263e0a609f820fb
kocanory <kocan9803@naver.com>
2024-09-15 17:12:02 +0900

    1719
    - 다익스트라
    - 우선순위 큐에 출발 지점 노드를 넣고, 인접한 노드를 순회하면서 거리의 최솟값을 갱신한다.
    - 우선순위 큐에서 가장 작은 누적 거리 값을 가진 노드 값을 가져온 다음, 그 노드와 인접한 노드의 출발 지점 부터의 거리값과 (누적 거리 값 + 현 노드에서 연결된 노드까지의 거리값) 비교하여 최솟값을 갱신한다.
    - 최솟값을 갱신할 때 먼저 거쳐야 할 노드 또한 갱신해야한다. 탐색한 현 노드가 출발지점인 경우 다음 노드 값을 저장하고, 그렇지 않은 경우 현 노드가 거친 노드 값을 저장한다.
    - 모든 탐색이 끝나면 거쳐야 할 노드 값을 출력한다.

---
+++ gold 3/1719.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+
+using namespace std;
+
+int n, m;
+vector<vector<int>> dist, ans;
+unordered_map<int, vector<tuple<int, int>>> graph;
+
+void dijkstra(int start) {
+    priority_queue<tuple<int, int>> pq;
+    pq.push({0, start});
+    dist[start][start] = 0;
+
+    while (!pq.empty()) {
+        auto[cost, now] = pq.top(); pq.pop();
+        cost = -cost;
+        if(dist[start][now] < cost) continue;
+        for(auto &n : graph[now]) {
+            auto [next_node, next_cost] = n;
+            next_cost += cost;
+            if(dist[start][next_node] >= next_cost) {
+                dist[start][next_node] = next_cost;
+                pq.push({-next_cost, next_node});
+                if(now == start) {
+                    ans[start][next_node] = next_node;
+                }
+                else {
+                    ans[start][next_node] = ans[start][now];
+                }
+            }
+        }
+    }
+    for(int i = 1;i <= n;i++) {
+        if(i == start) cout << "- ";
+        else cout << ans[start][i] << " ";
+    }
+    cout << "\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    dist.assign(n + 1, vector<int>(n + 1, 1e9));
+    ans.assign(n + 1, vector<int>(n + 1, 0));
+
+    for(int i = 0;i < m;i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
+    }
+
+    for(int i = 1;i <= n;i++) {
+        dijkstra(i);
+    }
+    return 0;
+}
