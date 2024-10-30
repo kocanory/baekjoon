dd221799d85c39ad027d8527b4781d3e2def5688
kocan <kocan9803@naver.com>
2024-10-30 14:15:28 +0900

    9661
    - 서로 돌을 가져가다가 남은 돌이 없거나, 2개가 남으면 후공(CY)이 이긴다.
    - 서로 돌을 가져가는 개수는 5의 배수마다 이루어지므로 5를 나누었을때 나머지를 확인한다.

---
+++ gold 2/9661.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);    
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    
+    n %= 5;
+    if(n == 0 || n == 2) cout << "CY" << "\n";
+    else cout << "SK" << "\n";
+
+    return 0;
+}
