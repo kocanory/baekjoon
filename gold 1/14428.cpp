fa45ecf1472449d6400f3985a2614b3c309cf71b
kocanory <kocan9803@naver.com>
2024-12-12 11:46:30 +0900

    14428
    - 세그먼트 트리
    - 입력받은 수와 인덱스를 pair로 구성하고, min 연산으로 최솟값을 갖는 인덱스를 노드로 구성한다.
    - update 함수를 통해 범위 바깥에 있는 수는 그대로 두고, 범위 안에 있는 수 만을 변경하고 최솟값을 비교하여 갱신한다.
    - find 함수를 통해 루트에서 리프노드로 내려가면서 범위 안에 속하는 구간은 그 구간 내 값과 인덱스를 반환하고, 그렇지 않으면 자식 노드로부터 최솟값을 갖는 값과 인덱스를 가져와 비교하고 최솟값을 반환한다.

---
+++ gold 1/14428.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int n, m;
+vector<int> arr;
+vector<pair<int, int>> segTree;
+
+pair<int, int> init(int start, int end, int nodeNum) {
+    if (start == end) return segTree[nodeNum] = {arr[start], start};
+    int mid = (start + end) / 2;
+    return segTree[nodeNum] = min(init(start, mid, nodeNum * 2), init(mid + 1, end, nodeNum * 2 + 1));
+}
+
+pair<int, int> update(int start, int end, int nodeNum, int target, int val) {
+    if (target < start || target > end) return segTree[nodeNum];
+    if (start == end)  return segTree[nodeNum] = {val, start};
+    int mid = (start + end) / 2;
+    return segTree[nodeNum] = min(update(start, mid, nodeNum * 2, target, val), update(mid + 1, end, nodeNum * 2 + 1, target, val));
+}
+
+pair<int, int> find(int left, int right, int start, int end, int nodeNum) {
+    if (left > end || right < start) return {1e9, 1e9};
+    if (left <= start && end <= right) return segTree[nodeNum];
+    int mid = (start + end) / 2;
+    return min(find(left, right, start, mid, nodeNum * 2), find(left, right, mid + 1, end, nodeNum * 2 + 1));
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n + 1, 0);
+    segTree.assign(1 << (int)ceil(log2(n)) + 1, pair<int, int>());
+
+    for (int i = 1;i <= n;i++) cin >> arr[i];
+    init(1, n, 1);
+
+    cin >> m;
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) update(1, n, 1, b, c);
+        else cout << find(b, c, 1, n, 1).second << "\n";
+    }
+    return 0;
+}
