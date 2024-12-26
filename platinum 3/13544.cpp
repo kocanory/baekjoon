9460a26a7b3690b6d44ebc63bd95761107f12343
kocan <kocan9803@naver.com>
2024-12-26 17:43:57 +0900

    13544
    - 세그먼트 트리
    - 트리를 2차원 벡터로 구성하여, 리프 노드로부터 위로 올라오면서 자식 노드에 저장된 벡터를 현재 탐색 중인 벡터에 insert 함수로 삽입하고, 정렬한다.
    - 구간에서 k보다 큰 원소의 개수를 구할 떄에는 upper_bound 함수를 사용해서 k보다 큰 원소의 인덱스를 구하고(k보다 작은 원소의 개수), 현재 노드에 저장된 원소의 개수에서 인덱스 값을 뺀다.
    - find 함수 인자에 이전 쿼리 값과 입력 값의 xor 값을 전달한다.

---
+++ platinum 3/13544.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, back = 0;
+vector<int> arr;
+vector<vector<int>> tree;
+
+vector<int> init(int start, int end, int nodeNum){
+    if(start == end){
+        tree[nodeNum].push_back(arr[start]);
+        return tree[nodeNum];
+    }
+    int mid = (start + end) / 2;
+    tree[nodeNum] = init(start, mid, nodeNum * 2);
+    vector<int> rc = init(mid + 1, end, nodeNum * 2 + 1);
+    tree[nodeNum].insert(tree[nodeNum].end(), rc.begin(), rc.end());
+    sort(tree[nodeNum].begin(), tree[nodeNum].end());
+    return tree[nodeNum];
+}
+
+int find(int left, int right, int val, int start, int end, int nodeNum){
+    if(end < left || start > right) return 0;
+    if(left <= start && end <= right) return tree[nodeNum].size() - distance(tree[nodeNum].begin(), upper_bound(tree[nodeNum].begin(), tree[nodeNum].end(), val));
+    int mid = (start + end) / 2;
+    return find(left, right, val, start, mid, nodeNum * 2) + find(left, right, val, mid + 1, end, nodeNum * 2 + 1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    arr.assign(n + 1, 0);
+    tree.assign(1 << ((int)ceil(log2(n)) + 1), vector<int>());
+    
+    for(int i = 1;i <= n;i++) cin >> arr[i];
+    init(1, n, 1);
+    
+    cin >> m;
+    while(m--){
+        int i, j, k;
+        cin >> i >> j >> k;
+        cout << (back = find(i ^ back, j ^ back, k ^ back, 1, n, 1)) << "\n";
+    }
+    return 0;
+}
