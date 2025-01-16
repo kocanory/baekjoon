21b25123979f259a0839536517e243d033e90e19
kocanory <kocan9803@naver.com>
2025-01-16 15:10:58 +0900

    1168
    - 세그먼트 트리
    - index를 k 만큼, 인원 수를 1명씩 줄여가면서 해당 index에 존재하는 번호를 출력한다.
    - index를 k - 1만큼 더하는 이유는 1명을 제외하면 그 자리에 다음 사람이 오기 때문에 실질적으로 다음 k - 1번째 사람을 구하게 되는 것이다.
    - 자식 노드에 저장된 수와 인덱스를 비교해서 왼쪽 자식 노드의 수가 인덱스보다 크거나 같다면 왼쪽 자식 노드로 탐색하고, 그렇지 않으면 오른족 자식노드로 탐색한다.

---
+++ platinum 3/1168.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int n, k, index = 1;
+vector<int> tree;
+
+int init(int start, int end, int nodeNum){
+    if(start == end) return tree[nodeNum] = 1;
+    int mid = (start + end) / 2;
+    return tree[nodeNum] = init(start, mid, nodeNum * 2) + init(mid + 1, end, nodeNum * 2 + 1);
+}
+
+int update(int start, int end, int nodeNum, int target){
+    if(target < start || target > end) return tree[nodeNum];
+    if(start == end) return tree[nodeNum] = 0;
+    int mid = (start + end) / 2;
+    return tree[nodeNum] = update(start, mid, nodeNum * 2, target) + update(mid + 1, end, nodeNum * 2 + 1, target);
+}
+
+int find(int start, int end, int nodeNum, int idx){
+    if(start == end) return start;
+    int mid = (start + end) / 2;
+    if(tree[nodeNum * 2] >= idx) return find(start, mid, nodeNum * 2, idx);
+    return find(mid + 1, end, nodeNum * 2 + 1, idx - tree[nodeNum * 2]);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> k;
+    tree.assign(1 << (int)ceil(log2(n)) + 1, 0);
+    
+    init(1, n, 1);
+    
+    cout << "<";
+    for(int i = 0;i < n;i++){
+        int size = n - i;
+        index += k - 1;
+        
+        if(index % size == 0) index = size;
+        else if(index > size) index %= size;
+        
+        int num = find(1, n, 1, index);
+        update(1, n, 1, num);
+        cout << num;
+        if(i < n - 1) cout << ", ";
+    }
+    cout << ">";
+    return 0;
+}
