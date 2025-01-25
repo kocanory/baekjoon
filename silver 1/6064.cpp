49e9a9d5ed2153a1c9794ffc0070875f4a5d53c1
kocanory <kocan9803@naver.com>
2025-01-25 20:11:09 +0900

    6064
    - m의 배수 + x = n의 배수 + y이다. 따라서, (m의 배수 + x - y)가 n의 배수인지 확인한다.
    - x, y가 m, n와 같을 수 있으므로 그대로 m, n으로 나머지 연산을 하면 실제 결과와 다르게 나온다. 따라서 (m의 배수 + x - y)에 n을 더하고 나머지 연산을 하여 0이 나오는지 확인해야 한다.
    - 배수는 0 ~ n - 1까지 확인한다. n 이상은 0 ~ n - 1 값이 순환한다.
    - 모든 배수를 확인했는데도 찾을 수 없다면 -1을 출력한다.

---
+++ silver 1/6064.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int t, n, m, x, y;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> t;
+    while(t--){
+        cin >> m >> n >> x >> y;
+        bool flag = true;
+        for(int i = 0;i < n;i++){
+            if((i * m + x - y + n) % n == 0){
+                cout << i * m + x << "\n";
+                flag = false;
+                break;
+            }
+        }
+        if(flag) cout << -1 << "\n";
+    }
+    return 0;
+}
