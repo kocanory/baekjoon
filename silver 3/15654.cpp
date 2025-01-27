cc0f7a9d896893d71e231b669a80f11bef6c9d1f
kocanory <kocan9803@naver.com>
2025-01-27 13:58:13 +0900

    15654
    - dfs
    - 벡터에 저장된 원소를 탐색하면서, 만약 방문하지 않은 원소라면 그 원소를 다른 벡터에 저장하고, 다른 원소를 탐색하고 벡터에 저장하는 과정을 반복한다. 이 과정을 dfs로 구현했다.
    - 벡터에 저장된 원소의 개수가 m이라면 그 벡터를 저장한다.
    - 조합이 저장된 벡터를 정렬한다.
    - 조합이 저장된 벡터를 순회하면서 벡터에 저장된 조합을 출력한다.

--- silver 3/15654.cpp
+++ silver 3/15654.cpp
@@ -9,39 +9,42 @@ vector<int> arr, temp;
 vector<bool> visited;
 vector<vector<int>> ans;
 
-void dfs(int idx) {
-    if (temp.size() >= m) {
+void dfs(){
+    if(temp.size() >= m){
         ans.push_back(temp);
         return;
     }
-    for (int i = 0; i < arr.size(); i++) {
-        if (!visited[i]) {
+    for(int i = 0;i <  n;i++){
+        if(!visited[i]){
             visited[i] = true;
             temp.push_back(arr[i]);
-            dfs(i + 1);
-            visited[i] = false;
+            dfs();
             temp.pop_back();
+            visited[i] = false;
         }
     }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n >> m;
     arr.assign(n, 0);
     visited.assign(n, false);
-
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
-    dfs(0);
+    
+    for(int i = 0;i <n;i++) cin >> arr[i];
+    
+    dfs();
+    
     sort(ans.begin(), ans.end());
-    for (auto a : ans) {
-        for (auto num : a)
+    
+    for(auto a : ans){
+        for(auto num : a)
             cout << num << " ";
-        cout << "\n";
+        cout <<"\n";
     }
     return 0;
 }
