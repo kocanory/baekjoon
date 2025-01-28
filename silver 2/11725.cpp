b3236045decec28dab7942c4bd67b052f31e08a5
kocanory <kocan9803@naver.com>
2025-01-28 16:34:19 +0900

    11725
    - dfs
    - 1번 노드부터 리프 노드까지 이동한 다음 거슬러 올라오면서 자식 노드의 부모를 현재 노드로 지정한다.

--- silver 2/11725.cpp
+++ silver 2/11725.cpp
@@ -5,35 +5,38 @@ using namespace std;
 
 int n;
 vector<vector<int>> arr;
-vector<bool> visited;
-vector<int> parent;
+vector<int> visited, parent;
 
-void dfs(int now) {
-    for (auto next : arr[now]) {
-        if (visited[next]) continue;
-        visited[next] = true;
+void dfs(int now){
+    for(auto next : arr[now]){
+        if(visited[next]) continue;
+        visited[next] = 1;
         dfs(next);
         parent[next] = now;
     }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     cin >> n;
     arr.assign(n + 1, vector<int>());
-    visited.assign(n + 1, false);
-    parent.assign(n + 1, -1);
-
-    for (int i = 0;i < n - 1;i++) {
+    visited.assign(n + 1, 0);
+    parent.assign(n + 1, 0);
+    
+    for(int i = 0;i < n - 1;i++){
         int a, b;
         cin >> a >> b;
         arr[a].push_back(b);
         arr[b].push_back(a);
     }
+    
+    visited[1] = 1;
     dfs(1);
-    for (int i = 2;i <= n;i++) cout << parent[i] << "\n";
+    
+    for(int i = 2;i <= n;i++) cout << parent[i] << "\n";
     return 0;
 }
