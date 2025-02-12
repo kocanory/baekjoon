1bddf3a5e00b213baa95d9a6ef378cfe33a06984
kocan <kocan9803@naver.com>
2025-02-12 11:19:26 +0900

    15665
    - dfs
    - 배열을 정렬한다.
    - 현재 탐색중인 원소를 벡터에 따로 저장하고, 처음 원소부터 탐색을 새로 시작한다. 만약 원소를 m개 저장했다면 원소가 저장된 벡터를 따로 저장한다.
    - 원소가 저장된 벡터를 정렬한 다음 중복된 것은 제거한다.
    - 원소가 저장된 벡터의 벡터를 순회하면서 원소를 출력한다.

---
+++ silver 2/15665.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> arr, temp;
+vector<vector<int>> ans;
+
+void dfs(){
+    if(temp.size() >= m){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i = 0;i < n;i++){
+        temp.push_back(arr[i]);
+        dfs();
+        temp.pop_back();
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    arr.assign(n, 0);
+
+    for(int i = 0;i < n;i++) cin >> arr[i];
+
+    dfs();
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+    for(auto &a : ans){
+        for(auto &num : a)
+            cout << num << " ";
+        cout << "\n";
+    }
+    return 0;
+}
