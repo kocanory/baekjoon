f9c062ffd57c1506313837887aad95cec16830c9
kocan <kocan9803@naver.com>
2025-02-13 16:14:01 +0900

    1914
    - 1번 장대에 있는 n개의 원판을 3번 장대로 모두 옮기려면 1) n - 1개의 원판을 2번 장대로 이동, 2) 맨 밑에 있던 원판을 3번째 장대로 이동, 3) 2번 장대에 있던 n - 1개의 원판을 3번째 장대로 옮겨야 한다. 이를 식으로 나타내면 f(x) = 2 * f(x - 1) - 1 = 2 ^ n - 1 이다.
    - 이때, n이 100이하이므로 일반 정수형으로 구할 경우 오버플로우가 발생한다. 따라서 2 ^ n을 문자열로 변환한다음 -1을 한다.
    - 위에 설명한 하노이 구하는 방법을 재귀함수를 사용해서 구현한다.

---
+++ gold 5/1914.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n;
+
+void hanoi(int cnt, int start, int mid, int end){
+    if(cnt == 1){
+        cout << start << " " << end << "\n";
+        return;
+    }
+    hanoi(cnt - 1, start, end, mid);
+    cout << start << " " << end << "\n";
+    hanoi(cnt - 1, mid, start, end);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    
+    string num = to_string(pow(2, n));
+    num = num.substr(0, num.find('.'));
+    num[num.length() - 1] -= 1;
+    
+    cout << num << "\n";
+    
+    if(n <= 20) hanoi(n, 1, 2, 3);
+    return 0;
+}
