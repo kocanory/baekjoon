662609acded88bbe6e876ee4f786721799c03602
kocanory <kocan9803@naver.com>
2025-02-26 20:04:10 +0900

    20168
    - 다익스트라
    - 노드 간의 거리 합을 비교하는 것이 아닌 노드 간의 거리 중 중간에 거리 긴 부분을 비교한다.
    - 우선 순위 큐에 현재 가장 긴 거리, 거리의 합, 현재 노드 값을 저장하고, 인접한 노드를 탐색하면서 가장 긴 거리와 인접한 노드 간의 거리를 비교해서 가장 긴 거리를 저장한 후 저장한 거리가 인접한 노드에 저장된 값보다 작으면 갱신하고 우선순위 큐에 저장한다.
    - 모든 탐색이 끝난 후 도착지에서의 값이 갱신 되었으면 갱신된 값을 출력하고, 갱신되지 않았다면 -1을 출력한다.

---
+++ gold 5/20168.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n, m, a, b, c;
+unordered_map<int, vector<pair<int, int>>> graph;
+priority_queue<tuple<int, int, int>> pq;
+vector<int> dist;
+
+void dijkstra(){
+    pq.push({0, 0, a});
+    dist[a] = 0;
+    
+    while(!pq.empty()){
+        auto [Max, cost, now] = pq.top(); pq.pop();
+        Max = -Max;
+        if(dist[now] < Max) continue;
+        for(auto [next, n_cost] : graph[now]){
+            int n_max = max(Max, n_cost);
+            n_cost += cost;
+            if(n_cost > c) continue;
+            if(n_max < dist[next]){
+                dist[next] = n_max;
+                pq.push({-n_max, n_cost, next});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m >> a >> b >> c;
+    dist.assign(n + 1, 1e9);
+    
+    while(m--){
+        int f, t, d;
+        cin >> f >> t >> d;
+        graph[f].push_back({t, d});
+        graph[t].push_back({f, d});
+    }
+
+    dijkstra();
+    cout << (dist[b] == 1e9 ? -1 : dist[b]) << "\n";
+    return 0;
+}
