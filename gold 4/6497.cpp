a228a2e29ed2a942d8fc0b7a3786c839a9d13740
kocanory <kocan9803@naver.com>
2025-07-20 14:14:39 +0900

    6497
    - 크루스칼
    - 입력받은 간선들을 가중치를 기준으로 오름차순으로 정렬한다. 그 후 작은 가중치부터 순회하여 만약 해당 간선을 선택했을 때 사이클이 형성되지 않는다면(두 정점의 최상위 정점이 같지 않다면) 두 정점을 연결하고, 그렇지 않으면 해당 가중치를 더해나간다.
    - 정점 연결과 탐색의 경우 union-find 연산을 사용한다.

---
+++ gold 4/6497.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int m, n;
+vector<int> parent;
+
+int find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
+}
+
+void join(int x, int y) {
+    x = find(x), y = find(y);
+    if (x != y)
+        parent[max(x, y)] = min(x, y);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true) {
+        cin >> m >> n;
+        if (m == 0 && n == 0) break;
+
+        vector<tuple<int, int, int>> arr;
+        parent.assign(m, 0);
+        for (int i = 0;i < m;i++) parent[i] = i;
+
+        for (int i = 0;i < n;i++) {
+            int x, y, z;
+            cin >> x >> y >> z;
+            arr.push_back({z, x, y});
+        }
+        sort(arr.begin(), arr.end());
+
+        int ans = 0;
+        for (auto [z, x, y] : arr) {
+            x = find(x), y = find(y);
+            if (x != y) join(x, y);
+            else ans += z;
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
