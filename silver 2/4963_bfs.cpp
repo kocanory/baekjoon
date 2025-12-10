fd215fddd07851ef896d6153ca9d9a5c03dbe61c
kocanory <kocan9803@naver.com>
2025-12-10 13:37:46 +0900

    4963_bfs
    - bfs
    - 인근에 땅이 있는, 방문하지 않은 곳을 큐에 넣고 탐색한다.
    - bfs 연산을 시작할 때마다 카운트를 증가시키고, 모든 자리를 탐색했다면 카운트를 출력한다.

---
+++ silver 2/4963_bfs.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m, cnt;
+vector<vector<int>> arr, check;
+vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+void bfs(int i, int j) {
+    queue<pair<int, int>> q;
+    q.push({i, j});
+    check[i][j] = true;
+
+    while (!q.empty()) {
+        auto [x, y] = q.front(); q.pop();
+        for (auto [dx, dy] : dir) {
+            int nx = x + dx, ny = y + dy;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || check[nx][ny] || !arr[nx][ny]) continue;
+            check[nx][ny] = true;
+            q.push({nx, ny});
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true) {
+        cin >> m >> n;
+        if (n == 0 && m == 0) break;
+        arr.assign(n, vector(m, 0));
+        check.assign(n, vector(m, 0));
+
+        for (int i = 0;i < n;i++)
+            for (int j = 0;j < m;j++)
+                cin >> arr[i][j];
+
+        cnt = 0;
+        for (int i = 0 ;i <n;i++)
+            for (int j = 0;j < m;j++)
+                if (!check[i][j] && arr[i][j]) {
+                    cnt++;
+                    bfs(i, j);
+                }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
