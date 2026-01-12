1a6561a1a354727764f11228ed501ba6549be694
kocanory <kocan9803@naver.com>
2026-01-12 12:49:56 +0900

    18353_dp
    - dp
    - 앞에 있는 수가 뒤에 있는 수보다 큰 경우 앞에 있는 수까지의 최장 증가 수열 길이 + 1과 뒤에 있는 수까지의 최장 증가 수열과 비교하여 큰 길이로 갱신한다.
    - 모든 탐색이 끝나면 n에서 최장 증가 수열 길이를 뺀 값을 출력한다.

---
+++ silver 2/18353_dp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> arr, lis;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n, 0);
+
+    for (auto &a : arr)
+        cin >> a;
+
+    reverse(arr.begin(), arr.end());
+    for (auto &a : arr){
+        if (lis.empty() || lis.back() < a) lis.push_back(a);
+        else{
+            auto pos = lower_bound(lis.begin(), lis.end(), a);
+            *pos = a;
+        }
+    }
+
+    cout << n - lis.size();
+    return 0;
+}
