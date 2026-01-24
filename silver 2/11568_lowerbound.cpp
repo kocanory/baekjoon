0e35d29ad8e670eea35a16186ae0fb77dfa27cb3
kocanory <kocan9803@naver.com>
2026-01-24 11:12:18 +0900

    11568_lowerbound
    - 이분탐색
    - 각 원소를 탐색하면서 해당 원소가 최장 증가 수열 길이에 들어갈 수 있는 위치를 lower_bound 함수를 사용해 찾아낸다. 그 후 해당 위치에 해당 원소를 저장한다.
    - 모든 탐색이 끝나면 최장 증가 수열 길이를 출력한다.

---
+++ silver 2/11568_lowerbound.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> arr, lis;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n, 0);
+
+    for(int &a : arr) {
+        cin >> a;
+        if (lis.empty() || lis.back() < a) lis.push_back(a);
+        else {
+            auto pos = lower_bound(lis.begin(), lis.end(), a);
+            *pos = a;
+        }
+    }
+    cout << lis.size();
+}
