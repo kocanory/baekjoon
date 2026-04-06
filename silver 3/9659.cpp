aa16f1c6953607dcf5995d136636ce224b844c45
kocan <kocan9803@naver.com>
2026-04-06 17:16:19 +0900

    9659
    - 돌의 개수 따라 승리자를 판별해보면 짝수면 CY, 홀수면 SK이 승리하게 된다. 따라서 짝수면 CY, 홀수면 SK를 출력한다.

---
+++ silver 3/9659.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+long long n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    cout << (n % 2 ? "SK" : "CY");
+
+    return 0;
+}
