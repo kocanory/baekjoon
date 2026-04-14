93cf58274b9ced6b88763994ccc9e9f9880d6a9e
kocanory <kocan9803@naver.com>
2026-04-14 16:57:55 +0900

    10972
    - 뒤에서부터 순회하면서 내림차순이 끝나는 지점을 탐색한다. 그 후 내림차순 구간에서 해당 지점보다 큰 수를 찾아 서로 교환한다. 그 후 내림차순 구간을 오른차순으로 정렬한다. 그 후 순열을 출력한다.
    - 만약 입력받은 순열이 내림차순으로 되어있다면 -1을 출력한다.

---
+++ silver 3/10972.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, i, j;
+vector<int> arr;
+
+void check() {
+    i = n - 1;
+    while (i > 0 && arr[i - 1] > arr[i]) i--;
+    if (i == 0){
+        cout << -1;
+        return;
+    }
+
+    j = n - 1;
+    while (arr[i - 1] > arr[j]) j--;
+    swap(arr[i - 1], arr[j]);
+
+    j = n - 1;
+    while (i < j) {
+        swap(arr[i], arr[j]);
+        i++; j--;
+    }
+
+    for (int a : arr) cout << a << " ";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    arr.assign(n, 0);
+
+    for (int &a : arr) cin >> a;
+
+    check();
+
+    return 0;
+}
